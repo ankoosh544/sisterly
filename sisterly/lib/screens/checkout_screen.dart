bc20942b3cf97306715e95921983ed235b7df3ee
Sebastiano Sartor <s@sebsrt.xyz>
2021-11-19 15:50:08 +0100

    checkout1

---
+++ sisterly/lib/screens/checkout_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckoutScreen extends StatefulWidget {
+  CheckoutScreen({Key key}) : super(key: key);
+
+  @override
+  _CheckoutScreenState createState() => _CheckoutScreenState();
+}
+
+class _CheckoutScreenState extends State<CheckoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
